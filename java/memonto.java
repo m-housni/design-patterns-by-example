f5a4e6c9d36d29dec165907db0ddf4fcab761a96
m-housni <mohamed.housni@um5s.net.ma>
2023-05-29 14:07:45 +0100

    Update memonto.java

--- java/memonto.java
+++ java/memonto.java
@@ -1,3 +1,12 @@
+/** 
+GitHub Copilot
+Sure! Here's an example implementation of the Memento pattern in Java:
+
+In this example, we have an Originator class (TextEditor) that has a text field and methods to set and get the text, as well as to save and restore the state of the text using a Memento object. The Memento class has a text field and a method to get the text. The Caretaker class (TextEditorHistory) maintains a stack of Memento objects and has methods to save the state of the TextEditor and to undo the last state change.
+
+In the example usage, we create a TextEditor and a TextEditorHistory, set the text to "Hello, world!", save the state, set the text to "Goodbye, world!", save the state again, print the current text (which should be "Goodbye, world!"), undo the last state change, and print the text again (which should be "Hello, world!").
+*/
+
 // Originator class
 class TextEditor {
     private String text;
